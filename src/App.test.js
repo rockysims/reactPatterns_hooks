52f6a65619bf18298d5e5c70c41ddbb720255337
Rocky Sims <rocky.sims@gmail.com>
2020-03-23 14:46:54 -0700

    Blank create-react-app with the cruft removed.

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App component contains element div.App', () => {
+  const { container } = render(<App />);
+  const select = container.querySelector.bind(container);
+  const appDiv = select('div.App');
+  expect(appDiv).not.toBe(null);
 });
